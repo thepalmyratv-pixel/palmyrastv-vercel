6e4cd5c9914e729b0f6b437dcfde81a886a40175
Isravel <isravelkanagarajd@gmail.com>
2026-08-05 21:11:09 +0530

    Update Page

--- app/watch/[id]/page.tsx
+++ app/watch/[id]/page.tsx
@@ -5,16 +5,29 @@ export default async function VideoPage({
 }) {
   const { id } = await params;
 
+  const isLocal = id.startsWith("PTV");
+
   return (
     <main className="bg-black min-h-screen flex justify-center items-center p-4">
       <div className="w-full max-w-7xl aspect-video">
-        <iframe
-          className="w-full h-full rounded-xl"
-          src={`https://www.youtube.com/embed/${id}?autoplay=1`}
-          title="YouTube player"
-          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        />
+
+        {isLocal ? (
+          <video
+            className="w-full h-full rounded-xl"
+            src={`/videos/${id}.mp4`}
+            controls
+            autoPlay
+          />
+        ) : (
+          <iframe
+            className="w-full h-full rounded-xl"
+            src={`https://www.youtube.com/embed/${id}?autoplay=1`}
+            title="YouTube player"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
+        )}
+
       </div>
     </main>
   );
