1a949cb35e5fa4d6106c278749a71829fb9040ca
Isravel <isravelkanagarajd@gmail.com>
2026-08-05 22:29:20 +0530

    Update Pages

---
+++ lib/videos.ts
@@ -0,0 +1,24 @@
+export const videos = [
+  {
+    id: "etdqh4E5QI8",
+    type: "youtube",
+    title: "12 Objects You Can Never Touch",
+    description: "The World's Most Protected Treasures",
+  },
+
+  {
+    id: "PTVIntro",
+    type: "local",
+    title: "This is PalmyrasTV",
+    description: "Welcome to PalmyrasTV",
+    thumbnail: "/thumbnails/PTVIntro.jpg",
+  },
+
+  {
+    id: "bestSaves",
+    type: "youtube",
+    title: "Best Saves",
+    description: "FIFA World Cup",
+  },
+];
+
